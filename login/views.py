16df186265fb0171eb3c0fd5851a136a58778ebd
Santeri Nuotiomaa <santeri.nuotiomaa@tuni.fi>
2020-03-10 15:30:05 +0200

    save lisätty registerii

--- login/views.py
+++ login/views.py
@@ -20,10 +20,10 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('login-home')
     else:
         form = UserCreationForm()
-    form = UserCreationForm()
     return render(request, 'login/register.html', {'form': form})
