b050dc5ebb7767e423d3473e8bd57cbc6fe97295
Santeri Nuotiomaa <santeri.nuotiomaa@tuni.fi>
2020-03-10 15:22:37 +0200

    registeriä tehty etiäpäi

--- login/urls.py
+++ login/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='login-home'),
+    path('register/', views.register, name='register'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
+post =[
+    {
+        'nimi' : 'Santeri',
+        'paikkakunta' : 'Tampere',
+        'tauti' : 'korona'
+    }
+]
 def home (request):
-    return HttpResponse('<h1> Korona sivusto</h1>')
+    context = {
+        'post' :post
+    }
+    return render(request, 'login/home.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login-home')
+    else:
+        form = UserCreationForm()
+    form = UserCreationForm()
+    return render(request, 'login/register.html', {'form': form})
